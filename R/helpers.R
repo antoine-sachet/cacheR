15fb747cc882e4fc95f3d7a14fe97621aa0827c2
Antoine Sachet <antoine.sac@gmail.com>
2020-03-20 13:20:17 +0000

    Improve inspect_cache using data.tree package

--- R/helpers.R
+++ R/helpers.R
@@ -16,14 +16,19 @@ create_if_needed <- function(dir) {
   status
 }
 
-#' Show all files in a cached object.
+#' Show all files in a stored object.
 #'
-#' A simple wrapper around list.files.
-#' Useful for debugging.
+#' A simple convenience function, useful for debugging.
 #'
 #' @param path Path to a stored object.
 #'
 #' @export
 inspect_cache <- function(path) {
-  list.files(path, recursive = TRUE, all.files = TRUE)
+  if (requireNamespace("data.tree", quietly = TRUE)) {
+    files <- list.files(path, recursive = TRUE, all.files = TRUE, include.dirs = TRUE, full.names = TRUE)
+    data.tree::as.Node(data.frame(pathString = files))
+  } else {
+    message("Optional: install the 'data.tree' package to print a directory tree. Defaulting to file listing instead.")
+    list.files(path, recursive = TRUE, all.files = TRUE)
+  }
 }
